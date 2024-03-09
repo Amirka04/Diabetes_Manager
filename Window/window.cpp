f03cf23600fa3516e740d18259bafce8daeb1dc7
Amirka04 <kokovamir6@gmail.com>
2024-03-09 16:19:14 +0300

    Create window class ans cmake file

---
+++ Window/window.cpp
@@ -0,0 +1,5 @@
+//
+// Created by cyber_amirka on 09.03.24.
+//
+
+#include "window.h"
---
+++ Window/window.h
@@ -0,0 +1,19 @@
+//
+// Created by cyber_amirka on 09.03.24.
+//
+
+#ifndef WINDOW_H
+#define WINDOW_H
+
+#include <QMainWindow>
+
+
+class window : public QMainWindow{
+public:
+    explicit window(QWidget* parent = nullptr);
+
+};
+
+
+
+#endif //WINDOW_H
