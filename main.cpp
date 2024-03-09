654fb5502e48a88afaaca8c1f2640d8002e9a7ae
Amirka04 <kokovamir6@gmail.com>
2024-03-10 00:32:25 +0300

    Обновление содержимого и подключение Stylesheet к проекту

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
-#include <iostream>
 #include <QApplication>
+#include <QMainWindow>
+#include <QPushButton>
+#include <QFile>
+#include <QDebug>
+
 
 int main(int args, char**argv) {
     QApplication app(args, argv);
+
+    QFile file("Styles.qss");
+    file.open(QFile::ReadOnly);
+    QString style(file.readAll());
+    app.setStyleSheet(style);
+
+
 
 
     return app.exec();
