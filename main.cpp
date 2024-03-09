7e0ab11e65556b1ffaf2798cd7fadf2289125720
Amirka04 <kokovamir6@gmail.com>
2024-03-09 14:53:51 +0300

    Test working sqlite3 lib

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include "sqlite3.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    sqlite3 *db;
+    int result = sqlite3_open("test.db", &db);
+    printf("result = %d\n", result == SQLITE_OK);
+    sqlite3_close(db);
+
     return 0;
 }
