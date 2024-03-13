d06cfc99ed80ae543997a3218da7bf1c9d454b4c
Amirka04 <kokovamir6@gmail.com>
2024-03-13 15:59:52 +0300

    Add Class sqlServer

---
+++ SQLClass/sqlServer.cpp
@@ -0,0 +1,9 @@
+//
+// Created by cyber_amirka on 13.03.24.
+//
+
+#include "sqlServer.h"
+
+sqlServer::Connect::Connect() {
+
+}
---
+++ SQLClass/sqlServer.h
@@ -0,0 +1,24 @@
+//
+// Created by cyber_amirka on 13.03.24.
+//
+
+#ifndef SQLSERVER_H
+#define SQLSERVER_H
+
+#include "sqlite3.h"
+
+
+class sqlServer {
+    protected:
+        class Connect {
+            Connect();
+            ~Connect();
+        }connect;
+
+    public:
+
+};
+
+
+
+#endif //SQLSERVER_H
